541002fc65006f907f579ede7a33255081a0fdf3
Matheus Bitencourt Rangel Dantas <mdantas@br.ibm.com>
2022-05-22 12:34:23 -0300

    feat: Adding search query string route
    - Adding typing dependency and search query string route

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Union
 
 app = FastAPI()
 
@@ -6,3 +7,11 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/search")
+async def search(q: Union[str, None] = None):
+    return {"q": q}
